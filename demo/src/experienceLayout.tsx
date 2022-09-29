c65a8be0787e7621968110b87bfb8e213a2851f8
Zach Doster <dosterz97@gmail.com>
2022-09-29 16:01:57 -0500

    config for mac

--- demo/src/experienceLayout.tsx
+++ demo/src/experienceLayout.tsx
@@ -1,9 +1,20 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { Experience } from '@quarkworks-inc/avatar-webkit-rendering'
+import { Experience, ExperienceConfig } from '@quarkworks-inc/avatar-webkit-rendering'
 
 const ExperienceLayout = () => {
   const sceneCanvasRef = useRef<HTMLCanvasElement>()
   const [experience, setExperience] = useState(null)
+  const experienceParams = new URLSearchParams(window.location.search)
+
+  let config: ExperienceConfig = {
+    maxPixelRatio: 2
+  }
+
+  if (experienceParams.get('config') === 'mac') {
+    config = {
+      maxPixelRatio: 1
+    }
+  }
 
   useEffect(() => {
     const experience = new Experience({
@@ -11,9 +22,7 @@ const ExperienceLayout = () => {
       name: 'level1',
       modelType: 'rpm',
       modelUrl: 'https://d1a370nemizbjq.cloudfront.net/9f35ad26-5b47-4530-b245-be743317e094.glb',
-      config: {
-        maxPixelRatio: 2
-      }
+      config
       // modelType: 'chib',
       // modelUrl: '/models/chibs/1.glb'
     })
